ae5959f40550d5f2dea73bbf22d90454b21df9d1
Ahmed El-Adawy <ahmed.el-adawy@outlook.com>
2023-01-23 15:41:06 +0300

    unify menu in menu.js & add new procedures

---
+++ menu.js
@@ -0,0 +1,25 @@
+const menu = [
+    { href: "Tranning-needs.html", text: "الاحتياج التدريبي (الادارةالمختصة)" },
+    { href: "MyRequests.html", text: "طلباتي المرفوعة (الادارة المختصة)" },
+    { href: "AdminTrainingRequests.html", text: "ادارة طلبات البرامج (الادمن ومنسق التدريب المركز الرئيسي)" },
+    { href: "addNominee.html", text: "اضافة مرشحين" },
+    { href: "add-advert.html", text: "اضافة اعلان عن دورة" },
+    { href: "My-adv.html", text: "اعلاناتي المرسلة" },
+    { href: "Approve-Adv.html", text: "اعتماد ونشر الاعلانات" },
+    { href: "adv-details.html", text: "تفاصيل الاعلان والاعتماد" },
+    { href: "userlist.html", text: " ادارة التسجيل" },
+    { href: "adv.html", text: " الاعلان " },
+    { href: "register.html", text: " التسجيل في دورة " },
+    { href: "training-request.html", text: " طلب تدريب" },
+    { href: "My-TrainingRequests.html", text: "طلباتي للتدريب" },
+    { href: "Coordinator-TrainingRequests.html", text: "ادارة طلبات التدريب (منسق المركز ثم مدير المركز ثم المدير العام)" },
+    { href: "CreditHours-Request.html", text: "طلب اعتماد ساعات تعليمية لبرنامج تدريبي " },
+]
+
+window.onload = function () {
+    menu.forEach(item => {
+        let node = document.createElement("li");
+        node.innerHTML = `<a href="${item.href}">${item.text}</a>`;
+        document.getElementById("menu").appendChild(node);
+    });
+};
